422ced2424c8ff379be35e515e76f7989cdaa570
jitheshm <jitheshmjithooz@gmail.com>
2024-08-15 17:20:40 +0530

    bug fixed

--- src/middlewares/formValidation.ts
+++ src/middlewares/formValidation.ts
@@ -1,19 +1,17 @@
 import { NextFunction, Request, Response } from "express";
 import { validationResult } from "express-validator";
 import { FValidationError } from "../errors/ValidationError";
-import jwt from 'jsonwebtoken';
 
-export const formValidation = (req: Request & Partial<{ user: jwt.JwtPayload }>, res: Response, next: NextFunction) => {
+// Define a generic type parameter with a constraint
+export const formValidation = <T extends Record<string, any> | undefined>(req: Request<T>, res: Response, next: NextFunction) => {
     try {
-
         const result = validationResult(req);
         if (!result.isEmpty()) {
             throw new FValidationError(result.array(), 400);
-        }
-        else {
-            next()
+        } else {
+            next();
         }
     } catch (error) {
-        next(error)
+        next(error);
     }
 }
