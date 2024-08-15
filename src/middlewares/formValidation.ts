bba0f89277fa8ced39bdac635bfc667281b3fe61
jitheshm <jitheshmjithooz@gmail.com>
2024-08-15 16:40:42 +0530

    bugs fixed

--- src/middlewares/formValidation.ts
+++ src/middlewares/formValidation.ts
@@ -1,8 +1,9 @@
 import { NextFunction, Request, Response } from "express";
 import { validationResult } from "express-validator";
 import { FValidationError } from "../errors/ValidationError";
+import jwt from 'jsonwebtoken';
 
-export const formValidation = (req: Request, res: Response, next: NextFunction) => {
+export const formValidation = (req: Request & Partial<{ user: jwt.JwtPayload }>, res: Response, next: NextFunction) => {
     try {
 
         const result = validationResult(req);
